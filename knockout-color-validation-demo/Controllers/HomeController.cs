0de690933029ce231faab3c6a2a5308ac5e8fade
Trevor Reid <treid@bostonvalley.com>
2019-09-06 15:25:46 -0400

    Remove extraneous pages

--- knockout-color-validation-demo/Controllers/HomeController.cs
+++ knockout-color-validation-demo/Controllers/HomeController.cs
@@ -12,19 +12,5 @@ namespace knockout_color_validation_demo.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
